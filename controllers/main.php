7b2d81bc78795932e3813aff8e8422ff1d69a9db
Bhavik Maneck <bmaneck@gmail.com>
2013-08-29 00:59:29 +1000

    main cleaner template, header and footer seperate

--- controllers/main.php
+++ controllers/main.php
@@ -15,9 +15,13 @@ class Main_Controller
 	 */
 	public function main(array $getVars)
 	{
+		$header = new View_Model('header');
+		$header->assign('active_nav','home');
+		$footer = new View_Model('footer');
 		//create a new view and pass it our template
 		$view = new View_Model($this->template);
-		
+		$view->assign('header', $header->render(FALSE));
+		$view->assign('footer', $footer->render(FALSE));
 		//assign article data to view
 		//$view->assign('article' , $article);
 		
