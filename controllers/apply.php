b111930c69ef462790f0fee3a9fe95d503337633
Bhavik Maneck <bmaneck@gmail.com>
2013-09-30 04:34:39 +1000

    first page of form validates and doesn't move to next page until all fields filled out

--- controllers/apply.php
+++ controllers/apply.php
@@ -14,17 +14,27 @@ class Apply_Controller
 	 * @param array $getVars the GET variables posted to index.php
 	 */
 	public function main(array $getVars)
-	{
+	{	
+	
 		$header = new View_Model('header');
 		$header->assign('active_nav','apply');
 		$footer = new View_Model('footer');
+		
+		$errorAndValids = array('errors' => array(), 'valids' => array());
 		
 		if (isset($getVars['page']))
 		{
 			switch ($getVars['page'])
 			{	
 				case '2':
-					$this->template = 'apply-2';
+					$errorAndValids = $this->validatePage1($getVars);
+					if (count($errorAndValids['errors']) > 0 || count($errorAndValids['valids']) == 0)
+					{
+						$this->template = 'apply-2';
+					} else {
+						// move on to next page
+						$this->template = 'apply-3';
+					}
 				break;
 				
 				case '3':
@@ -62,6 +72,8 @@ class Apply_Controller
 		
 		}
 		
+		
+		
 		//create a new view and pass it our template
 		$view = new View_Model($this->template);
 		$view->assign('header', $header->render(FALSE));
@@ -69,7 +81,208 @@ class Apply_Controller
 		//assign article data to view
 		//$view->assign('article' , $article);
 		
+		//assign the errors array to the template data
+		$view->assign('errors', $errorAndValids['errors']);
+		$view->assign('valids', $errorAndValids['valids']);
+		
 		$view->render();
+	}
+	
+	private function validatePage1($fieldValues)
+	{
+		session_start();
+		
+		$errorAndValids = array('errors' => array(), 'valids' => array());
+		
+		if (isset($fieldValues['biztype']))
+		{
+			if ($fieldValues['biztype']  == 'select')
+			{
+				$errorAndValids['errors']['biztype'] = 'You must select a business type.';
+			} else {
+				$_SESSION['biztype'] = $fieldValues['biztype'];
+				$errorAndValids['valids']['biztype'] = $fieldValues['biztype'];
+			}
+		}
+		
+		if (isset($fieldValues['businessName']))
+		{
+			if (strlen($fieldValues['businessName']) == 0 )
+			{
+				$errorAndValids['errors']['businessName'] = 'Business name must not be empty.';
+			} elseif (strlen($fieldValues['businessName']) > 40) { 
+				$errorAndValids['errors']['businessName'] = 'Business name must be 40 characters or less.';
+			} else {
+				$_SESSION['businessName'] = $fieldValues['businessName'];
+				$errorAndValids['valids']['businessName'] = $fieldValues['businessName'];
+			}
+		}
+		
+		if (isset($fieldValues['tradingName']))
+		{
+			if (strlen($fieldValues['tradingName']) == 0 )
+			{
+				$errorAndValids['errors']['tradingName'] = 'Trading name must not be empty.';
+			} elseif (strlen($fieldValues['tradingName']) > 60) { 
+				$errorAndValids['errors']['tradingName'] = 'Trading name must be 60 characters or less.';
+			} else {
+				$_SESSION['tradingName'] = $fieldValues['tradingName'];
+				$errorAndValids['valids']['tradingName'] = $fieldValues['tradingName'];
+			}
+		}
+		
+		if (isset($fieldValues['yearBizStart']))
+		{
+			if (strlen($fieldValues['yearBizStart']) == 0 )
+			{
+				$errorAndValids['errors']['yearBizStart'] = 'Business start year must not be empty.';
+			} elseif (strlen($fieldValues['yearBizStart']) != 4) { 
+				$errorAndValids['errors']['yearBizStart'] = 'Business start year must be valid.';
+			} else {
+				$_SESSION['yearBizStart'] = $fieldValues['yearBizStart'];
+				$errorAndValids['valids']['yearBizStart'] = $fieldValues['yearBizStart'];
+			}
+		}
+		
+		if (isset($fieldValues['abn']))
+		{
+			if (strlen($fieldValues['abn']) == 0 )
+			{
+				$errorAndValids['errors']['abn'] = 'ABN/ACN not be empty.';
+			} elseif (strlen($fieldValues['abn']) > 11) { 
+				$errorAndValids['errors']['abn'] = 'ABN/ACN must be 11 digits or less.';
+			} else {
+				$_SESSION['abn'] = $fieldValues['abn'];
+				$errorAndValids['valids']['abn'] = $fieldValues['abn'];
+			}
+		}
+		
+		if (isset($fieldValues['operations']))
+		{
+			if (strlen($fieldValues['operations']) == 0 )
+			{
+				$errorAndValids['errors']['operations'] = 'Nature of operations not be empty.';
+			} elseif (strlen($fieldValues['operations']) > 200) { 
+				$errorAndValids['errors']['operations'] = 'Nature of operations must be 200 characters or less.';
+			} else {
+				$_SESSION['operations'] = $fieldValues['operations'];
+				$errorAndValids['valids']['operations'] = $fieldValues['operations'];
+			}
+		}
+		
+		if (isset($fieldValues['contactFirstName']))
+		{
+			if (strlen($fieldValues['contactFirstName']) == 0 )
+			{
+				$errorAndValids['errors']['contactFirstName'] = 'First name must not be empty.';
+			} elseif (strlen($fieldValues['contactFirstName']) > 15) { 
+				$errorAndValids['errors']['contactFirstName'] = 'First name must be 15 characters or less.';
+			} else {
+				$_SESSION['contactFirstName'] = $fieldValues['contactFirstName'];
+				$errorAndValids['valids']['contactFirstName'] = $fieldValues['contactFirstName'];
+			}
+		}
+		
+		if (isset($fieldValues['contactLastName']))
+		{
+			if (strlen($fieldValues['contactLastName']) == 0 )
+			{
+				$errorAndValids['errors']['contactLastName'] = 'Last name must not be empty.';
+			} elseif (strlen($fieldValues['contactLastName']) > 15) { 
+				$errorAndValids['errors']['contactLastName'] = 'Last name must be 15 characters or less.';
+			} else {
+				$_SESSION['contactLastName'] = $fieldValues['contactLastName'];
+				$errorAndValids['valids']['contactLastName'] = $fieldValues['contactLastName'];
+			}
+		}
+		
+		if (isset($fieldValues['inputPosition']))
+		{
+			if (strlen($fieldValues['inputPosition']) == 0 )
+			{
+				$errorAndValids['errors']['inputPosition'] = 'Position must not be empty.';
+			} elseif (strlen($fieldValues['inputPosition']) > 40) { 
+				$errorAndValids['errors']['inputPosition'] = 'Position must be 40 characters or less.';
+			} else {
+				$_SESSION['inputPosition'] = $fieldValues['inputPosition'];
+				$errorAndValids['valids']['inputPosition'] = $fieldValues['inputPosition'];
+			}
+		}
+		
+		if (isset($fieldValues['inputPhone']))
+		{
+			if (strlen($fieldValues['inputPhone']) == 0 )
+			{
+				$errorAndValids['errors']['inputPhone'] = 'Phone must not be empty.';
+			} elseif (strlen($fieldValues['inputPhone']) > 10) { 
+				$errorAndValids['errors']['inputPhone'] = 'Phone number must be 10 digits or less.';
+			} else {
+				$_SESSION['inputPhone'] = $fieldValues['inputPhone'];
+				$errorAndValids['valids']['inputPhone'] = $fieldValues['inputPhone'];
+			}
+		}
+		
+		if (isset($fieldValues['inputFax']))
+		{
+			if (strlen($fieldValues['inputFax']) == 0 )
+			{
+				$errorAndValids['errors']['inputFax'] = 'Fax must not be empty.';
+			} elseif (strlen($fieldValues['inputFax']) > 10) { 
+				$errorAndValids['errors']['inputFax'] = 'Fax number must be 10 digits or less.';
+			} else {
+				$_SESSION['inputFax'] = $fieldValues['inputFax'];
+				$errorAndValids['valids']['inputFax'] = $fieldValues['inputFax'];
+			}
+		}
+		
+		if (isset($fieldValues['inputMobile']))
+		{
+			if (strlen($fieldValues['inputMobile']) == 0 )
+			{
+				$errorAndValids['errors']['inputMobile'] = 'Mobile must not be empty.';
+			} elseif (strlen($fieldValues['inputMobile']) > 10) { 
+				$errorAndValids['errors']['inputMobile'] = 'Mobile number must be 10 digits or less.';
+			} else {
+				$_SESSION['inputMobile'] = $fieldValues['inputMobile'];
+				$errorAndValids['valids']['inputMobile'] = $fieldValues['inputMobile'];
+			}
+		}
+		
+		if (isset($fieldValues['inputEmail1']))
+		{
+			if (strlen($fieldValues['inputEmail1']) == 0 )
+			{
+				$errorAndValids['errors']['inputEmail1'] = 'Email address must not be empty.';
+			} elseif (strlen($fieldValues['inputEmail1']) > 80) { 
+				$errorAndValids['errors']['inputEmail1'] = 'Email address must be 80 characters or less.';
+			} else {
+				$_SESSION['inputEmail1'] = $fieldValues['inputEmail1'];
+				$errorAndValids['valids']['inputEmail1'] = $fieldValues['inputEmail1'];
+			}
+		}
+		
+		if (isset($fieldValues['creditLimit']))
+		{
+			if (strlen($fieldValues['creditLimit']) == 0 )
+			{
+				$errorAndValids['errors']['creditLimit'] = 'Credit limit must not be empty.';
+			} elseif (strlen($fieldValues['creditLimit']) > 5) { 
+				$errorAndValids['errors']['creditLimit'] = 'Credit limit must be 5 digits or less.';
+			} else {
+				$_SESSION['creditLimit'] = $fieldValues['creditLimit'];
+				$errorAndValids['valids']['creditLimit'] = $fieldValues['creditLimit'];
+			}
+		}
+		
+		
+		return $errorAndValids;
+	}
+	
+	//
+	private function splitErrorsAndValid($errorAndValids)
+	{
+	
+	
 	}
 }
 
---
+++ create_schema2.sql
@@ -0,0 +1,347 @@
+
+CREATE TABLE Account (
+                accountID INT NOT NULL,
+                userName VARCHAR(20) NOT NULL,
+                password VARCHAR(60) NOT NULL,
+                PRIMARY KEY (accountID)
+);
+
+
+CREATE TABLE CreditCardType (
+                creditCardTypeID INT NOT NULL,
+                creditCardType VARCHAR(25) NOT NULL,
+                PRIMARY KEY (creditCardTypeID)
+);
+
+
+CREATE TABLE JobType (
+                jobTypeID INT NOT NULL,
+                jobTypeDescription VARCHAR(50) NOT NULL,
+                PRIMARY KEY (jobTypeID)
+);
+
+
+CREATE TABLE Employee (
+                employeeID INT NOT NULL,
+                accountID INT NOT NULL,
+                jobTypeID INT NOT NULL,
+                empName VARCHAR(40) NOT NULL,
+                empPhone NUMERIC(10) NOT NULL,
+                empAddress VARCHAR(200) NOT NULL,
+                PRIMARY KEY (employeeID)
+);
+
+
+CREATE TABLE Information (
+                informationID INT NOT NULL,
+                employeeLastUpdate INT,
+                pageName VARCHAR(20) NOT NULL,
+                content LONGBLOB NOT NULL,
+                lastUpdated DATETIME NOT NULL,
+                PRIMARY KEY (informationID)
+);
+
+
+CREATE TABLE ProductType (
+                productTypeID INT NOT NULL,
+                productName VARCHAR(30) NOT NULL,
+                unitPrice DOUBLE NOT NULL,
+                PRIMARY KEY (productTypeID)
+);
+
+
+CREATE TABLE BusinessType (
+                businessTypeID INT AUTO_INCREMENT NOT NULL,
+                businessType VARCHAR(25) NOT NULL,
+                PRIMARY KEY (businessTypeID)
+);
+
+
+CREATE TABLE Application (
+                applicationID INT AUTO_INCREMENT NOT NULL,
+                employeeID INT NOT NULL,
+                contactFirstName VARCHAR(15) NOT NULL,
+                contactLastName VARCHAR(15) NOT NULL,
+                email VARCHAR(80) NOT NULL,
+                phone NUMERIC(10) NOT NULL,
+                position VARCHAR(40) NOT NULL,
+                fax NUMERIC(10) NOT NULL,
+                existingSupplierPhone NUMERIC(10) NOT NULL,
+                mobile NUMERIC(10) NOT NULL,
+                applicationStatus VARCHAR(10) NOT NULL,
+                applicationDate DATE NOT NULL,
+                creditLimit NUMERIC(5) NOT NULL,
+                existingFuelSupplier VARCHAR(40) NOT NULL,
+                PRIMARY KEY (applicationID)
+);
+
+
+CREATE TABLE BusinessDetails (
+                applicationID INT NOT NULL,
+                businessTypeID INT NOT NULL,
+                abn NUMERIC(11) NOT NULL,
+                businessStartYear INT NOT NULL,
+                businessName VARCHAR(40) NOT NULL,
+                operationsNaure VARCHAR(200) NOT NULL,
+                tradingName VARCHAR(60) NOT NULL,
+                PRIMARY KEY (applicationID)
+);
+
+
+CREATE TABLE FuelCards (
+                fuelCardID INT NOT NULL,
+                applicationID INT NOT NULL,
+                accountID INT NOT NULL,
+                cardHolderName VARCHAR(40) NOT NULL,
+                cardStatus VARCHAR(10) NOT NULL,
+                dateCreated DATE NOT NULL,
+                registrationNo VARCHAR(15) NOT NULL,
+                pinRequired BOOLEAN NOT NULL,
+                pin INT NOT NULL,
+                PRIMARY KEY (fuelCardID)
+);
+
+
+CREATE TABLE FuelCardProducts (
+                fuelCardID INT NOT NULL,
+                productTypeID INT NOT NULL,
+                PRIMARY KEY (fuelCardID, productTypeID)
+);
+
+
+CREATE TABLE FuelTransactions (
+                transactionID INT NOT NULL,
+                productTypeID INT NOT NULL,
+                fuelCardID INT NOT NULL,
+                totalPrice DOUBLE NOT NULL,
+                date DATE NOT NULL,
+                location VARCHAR(15) NOT NULL,
+                quantity INT NOT NULL,
+                time TIME NOT NULL,
+                PRIMARY KEY (transactionID)
+);
+
+
+CREATE TABLE BusinessPartners (
+                businessPartnetID INT NOT NULL,
+                applicationID INT NOT NULL,
+                partnerName VARCHAR(40) NOT NULL,
+                partnerPhone NUMERIC(10) NOT NULL,
+                partnerPostcode INT NOT NULL,
+                partnerAddress VARCHAR(200) NOT NULL,
+                partnerState VARCHAR(3) NOT NULL,
+                PRIMARY KEY (businessPartnetID)
+);
+
+
+CREATE TABLE PartnersAuthorization (
+                businessPartnetID INT NOT NULL,
+                driverLicenceNumber VARCHAR(20) NOT NULL,
+                dateOfBirth DATE NOT NULL,
+                signatureObtained BOOLEAN NOT NULL,
+                PRIMARY KEY (businessPartnetID)
+);
+
+
+CREATE TABLE Customer (
+                customerID INT NOT NULL,
+                applicationID INT NOT NULL,
+                accountID INT NOT NULL,
+                accountCreated DATE NOT NULL,
+                customerStatus VARCHAR(10) NOT NULL,
+                PRIMARY KEY (customerID)
+);
+
+
+CREATE TABLE CustomerAccReview (
+                accountReviewID INT NOT NULL,
+                customerID INT NOT NULL,
+                employeeID INT NOT NULL,
+                oldStatus VARCHAR(10) NOT NULL,
+                dateTime DATETIME NOT NULL,
+                newStatus VARCHAR(10) NOT NULL,
+                PRIMARY KEY (accountReviewID)
+);
+
+
+CREATE TABLE Payments (
+                paymentID INT NOT NULL,
+                customerID INT NOT NULL,
+                paymentMethod VARCHAR(20) NOT NULL,
+                paymentDate DATE NOT NULL,
+                amount DOUBLE NOT NULL,
+                PRIMARY KEY (paymentID)
+);
+
+
+CREATE TABLE CreditCardInfo (
+                creditCardID INT NOT NULL,
+                applicationID INT NOT NULL,
+                creditCardTypeID INT NOT NULL,
+                creditCardNo NUMERIC NOT NULL,
+                cardHolderName VARCHAR(40) NOT NULL,
+                monthlyPaymentDay INT NOT NULL,
+                expiryDate DATE NOT NULL,
+                PRIMARY KEY (creditCardID)
+);
+
+
+CREATE TABLE DirectDebitInfo (
+                directDebitID INT NOT NULL,
+                applicationID INT NOT NULL,
+                financialInstitution VARCHAR(40) NOT NULL,
+                cheque BOOLEAN NOT NULL,
+                accountNo NUMERIC NOT NULL,
+                bsb NUMERIC NOT NULL,
+                savings BOOLEAN NOT NULL,
+                accountName VARCHAR(40) NOT NULL,
+                PRIMARY KEY (directDebitID)
+);
+
+
+CREATE TABLE ApplicationReferences (
+                referenceID INT AUTO_INCREMENT NOT NULL,
+                applicationID INT NOT NULL,
+                refPhone NUMERIC(10) NOT NULL,
+                referenceName VARCHAR(40) NOT NULL,
+                PRIMARY KEY (referenceID)
+);
+
+
+ALTER TABLE Customer ADD CONSTRAINT account_customer_fk
+FOREIGN KEY (accountID)
+REFERENCES Account (accountID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE FuelCards ADD CONSTRAINT account_fuelcards_fk
+FOREIGN KEY (accountID)
+REFERENCES Account (accountID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE Employee ADD CONSTRAINT account_employee_fk
+FOREIGN KEY (accountID)
+REFERENCES Account (accountID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE CreditCardInfo ADD CONSTRAINT creditcardtype_creditcardinfo_fk
+FOREIGN KEY (creditCardTypeID)
+REFERENCES CreditCardType (creditCardTypeID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE Employee ADD CONSTRAINT jobtype_employee_fk
+FOREIGN KEY (jobTypeID)
+REFERENCES JobType (jobTypeID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE Application ADD CONSTRAINT employee_application_fk
+FOREIGN KEY (employeeID)
+REFERENCES Employee (employeeID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE CustomerAccReview ADD CONSTRAINT employee_customeraccreview_fk
+FOREIGN KEY (employeeID)
+REFERENCES Employee (employeeID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE Information ADD CONSTRAINT employee_information_fk
+FOREIGN KEY (employeeLastUpdate)
+REFERENCES Employee (employeeID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE FuelTransactions ADD CONSTRAINT producttype_fueltransactions_fk
+FOREIGN KEY (productTypeID)
+REFERENCES ProductType (productTypeID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE FuelCardProducts ADD CONSTRAINT producttype_fuelcardproducts_fk
+FOREIGN KEY (productTypeID)
+REFERENCES ProductType (productTypeID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE BusinessDetails ADD CONSTRAINT businesstype_businessdetails_fk
+FOREIGN KEY (businessTypeID)
+REFERENCES BusinessType (businessTypeID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE ApplicationReferences ADD CONSTRAINT application_references_fk
+FOREIGN KEY (applicationID)
+REFERENCES Application (applicationID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE DirectDebitInfo ADD CONSTRAINT directdebitinfo_application_fk
+FOREIGN KEY (applicationID)
+REFERENCES Application (applicationID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE CreditCardInfo ADD CONSTRAINT application_creditcardinfo_fk
+FOREIGN KEY (applicationID)
+REFERENCES Application (applicationID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE Customer ADD CONSTRAINT application_customer_fk
+FOREIGN KEY (applicationID)
+REFERENCES Application (applicationID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE BusinessPartners ADD CONSTRAINT application_businesspartners_fk
+FOREIGN KEY (applicationID)
+REFERENCES Application (applicationID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE FuelCards ADD CONSTRAINT application_fuelcards_fk
+FOREIGN KEY (applicationID)
+REFERENCES Application (applicationID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE BusinessDetails ADD CONSTRAINT application_businessdetails_fk
+FOREIGN KEY (applicationID)
+REFERENCES Application (applicationID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE FuelTransactions ADD CONSTRAINT fuelcards_fueltransactions_fk
+FOREIGN KEY (fuelCardID)
+REFERENCES FuelCards (fuelCardID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE FuelCardProducts ADD CONSTRAINT fuelcards_fuelcardproducts_fk
+FOREIGN KEY (fuelCardID)
+REFERENCES FuelCards (fuelCardID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE PartnersAuthorization ADD CONSTRAINT businesspartners_partnersauthorization_fk
+FOREIGN KEY (businessPartnetID)
+REFERENCES BusinessPartners (businessPartnetID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE Payments ADD CONSTRAINT customer_payments_fk
+FOREIGN KEY (customerID)
+REFERENCES Customer (customerID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE CustomerAccReview ADD CONSTRAINT customer_customeraccreview_fk
+FOREIGN KEY (customerID)
+REFERENCES Customer (customerID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
