6f06a9a28b214dafeb95d9ff963ab90b02d63afe
Bhavik Maneck <bmaneck@gmail.com>
2013-10-05 18:09:12 +1000

    added more functions for inserting application

--- models/apply.php
+++ models/apply.php
@@ -9,32 +9,110 @@ class Apply_Model {
         $this->database = new Database_Library;
     }
 	
-	public function add_user($username,$password) {
+	public function addApplication($firstName,$lastName,$email,$phone,$position,$fax,$existingSupplierPhone,$mobile,
+							 $creditLimit,$existingFuelSupplier) 
+	{
 		
-		$password = sha1($password);
-		$query = "INSERT INTO account (userName,password) VALUES (:username,:password)";
+		$query = "INSERT INTO application 
+				  (contactFirstName,contactLastName,email,phone,position,fax,existingSupplierPhone,
+				   mobile,applicationStatus,applicationDate,creditLimit,existingFuelSupplier) 
+				  VALUES (:firstName,:lastName,:email,:phone,:position,:fax,:existingSuppPhone,
+						  :mobile,'pending',NOW(),:creditLimit,:existingFuelSupplier)";
+		
+		// Application status must be 'pending','approved','rejected'
 		
 		$statement = $this->database->db->prepare($query);
-		$statement->bindValue(':username',$username);
-		$statement->bindValue(':password',$password);
+		$statement->bindValue(':firstName',$firstName);
+		$statement->bindValue(':lastName',$lastName);
+		$statement->bindValue(':email',$email);
+		$statement->bindValue(':phone',$position);
+		$statement->bindValue(':fax',$fax);
+		$statement->bindValue(':existingSuppPhone',$existingSupplierPhone);
+		$statement->bindValue(':mobile',$mobile);
+		$statement->bindValue(':creditLimit',$creditLimit);
+		$statement->bindValue(':existingFuelSupplier',$existingFuelSupplier);
 		$statement->execute();
 		$statement->closeCursor();
 		
-		return $this->database->db->lastInsertId('account');
+		return $this->database->db->lastInsertId('application');
 	}
 	
-	public function is_valid_login($username,$password) {
+	public function getBusinessTypeID($businessType)
+	{
+		$query = "SELECT businessTypeID FROM BusinessType WHERE businessType = :businessType";
 		
-		$password = sha1($password);
-		$query = "SELECT accountID FROM account WHERE userName = :username AND password = :password";
 		$statement = $this->database->db->prepare($query);
-		$statement->bindValue(':username',$username);
-		$statement->bindValue(':password',$password);
+		$statement->bindValue(':businessType',$businessType);
 		$statement->execute();
-		
-		$valid = ($statement->rowCount() == 1);
+		$result = $statement->fetch();
 		$statement->closeCursor();
-		return $valid;
+		
+		if (count($result) > 0 )
+		{
+			return $result['businessTypeID'];
+		} else {
+			return false;
+		}
+		
+	}
+	
+	public function addApplicationBusinessDetails($applicationID,$businessTypeID,$abn,$startYear,$busName,$operations,$tradingName)
+	{
+		$query = "INSERT INTO businessdetails
+				  VALUES (:applicationID,:businessType,:abn,:startYear,:name,:operations,:tradingName)";
+		
+		$statement = $this->database->db->prepare($query);
+		$statement->bindValue(':applicationID',$applicationID);
+		$statement->bindValue(':businessTypeID',$businessTypeID);
+		$statement->bindValue(':abn',$abn);
+		$statement->bindValue(':startYear',$busName);
+		$statement->bindValue(':operations',$operations);
+		$statement->bindValue(':tradingName',$tradingName);
+		$statement->execute();
+		$statement->closeCursor();
+	}
+	
+	public function addBusinessPartner($applicationID,$name,$phone,$postcode,$address,$state)
+	{
+		$query = "INSERT INTO businesspartners
+				  (applicationID,partnerName,partnerPhone,partnerPostcode,partnerAddress,partnerState)
+				  VALUES (:applicationID,:name,:phone,:postcode,:address,:state)";
+		
+		$statement = $this->database->db->prepare($query);
+		$statement->bindValue(':applicationID',$applicationID);
+		$statement->bindValue(':name',$name);
+		$statement->bindValue(':phone',$phone);
+		$statement->bindValue(':postcode',$postcode);
+		$statement->bindValue(':address',$address);
+		$statement->bindValue(':state',$state);
+		$statement->execute();
+		$statement->closeCursor();
+		
+		return $this->database->db->lastInsertId('businesspartners');	
+	}
+	
+	public function addFuelCards($applicationID,$name,$registration,$pinRequired)
+	{
+		$query = "INSERT INTO fuelcards
+				  (applicationID,accountID,cardHolderName,cardStatus,dateCreated,registrationNo,pinRequired,pin)
+				  VALUES (:applicationID,NULL,:name,'disabled',NOW(),:registration,:pinRequired,NULL)";
+				
+		//cardStatus can take values 'enabled','disabled','cancelled'
+		
+		$statement = $this->database->db->prepare($query);
+		$statement->bindValue(':applicationID',$applicationID);
+		$statement->bindValue(':name',$name);
+		$statement->bindValue(':registration',$registration
+		if ($pinRequired == 'yes')
+		{
+			$statement->bindValue(':pinRequired',TRUE);
+		} else {
+			$statement->bindValue(':pinRequired',FALSE);
+		}
+		$statement->execute();
+		$statement->closeCursor();
+		
+		return $this->database->db->lastInsertId('fuelcards');	
 	}
 
 }
