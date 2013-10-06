be7594a11f565d79e786c932782fd87b6352a3f6
Bhavik Maneck <bmaneck@gmail.com>
2013-10-06 20:19:58 +1000

    small improvements to employee management account

--- views/customer-change-pass.php
+++ views/customer-change-pass.php
@@ -1,8 +1,9 @@
 <p><b>Change your login password:</b></p>
   <br />
+<div class="col-xs-12 col-sm-6 col-md-12">
 <div class="row">
   <div class="col-md-4">
-	<label for="inputPhone" class="col-lg-2 control-label">Current Password</label>
+	<label for="inputPhone" class="control-label">Current Password</label>
   </div>
   <div class="col-md-4">
 	 <input type="password" class="form-control" id="inputPhone">
@@ -12,7 +13,7 @@
 <br />
 <div class="row spacing-under-small">
   <div class="col-md-4">
-	<label for="inputPhone" class="col-lg-2 control-label">New Password</label>
+	<label for="inputPhone" class="control-label">New Password</label>
   </div>
   <div class="col-md-4">
 	 <input type="password" class="form-control" id="inputPhone">
@@ -21,7 +22,7 @@
 
 <div class="row">
   <div class="col-md-4">
-	<label for="inputPhone" class="col-lg-2 control-label">Confirm New Password</label>
+	<label for="inputPhone" class="control-label">Confirm New Password</label>
   </div>
   <div class="col-md-4">
 	 <input type="password" class="form-control" id="inputPhone">
@@ -32,4 +33,5 @@
 	 <br />
 	 <a class="btn btn btn-primary" href="">Change &raquo;</a>
   </div>
+</div>
 </div>
